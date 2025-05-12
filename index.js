65b8f19b1323f9a7527d6a932bb6ec85d532287e
mahammadshariaralam <mahammadshariaralam@gmail.com>
2025-05-12 21:08:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let someValue: any = "এটি একটি স্ট্রিং";
+let strLength: number = (<string>someValue).length;
+
+let someValue: any = "এটি একটি স্ট্রিং";
+let strLength: number = (someValue as string).length;
